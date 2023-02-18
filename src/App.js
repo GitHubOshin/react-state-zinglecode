fff4b2de1a05543c3e98e77e2f43aff0a823aa32
OSHIN <izkp44@gmail.com>
2023-02-18 22:36:44 +0700

    State x Array (Add)

--- src/App.js
+++ src/App.js
@@ -2,44 +2,81 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [note, setNote] = useState({
+  const startNote = {
     content: '',
     author: ''
-  })
-  function onNoteValueChange(e) {
-    console.log(e.target)
-    const { name, value } = e.target
-    setNote((prevNote) => {
-      return { ...prevNote, [name]: [value] }
-    })
   }
+
+  // States
+
+  const [note, setNote] = useState(startNote)
+  const [allNotes, setAllNotes] = useState([])
+
+  // Functions
+
+  function onNoteValueChange(e) {
+    // console.log(e.target)
+    const { name, value } = e.target
+    // console.log('name: ' + name, ' value: ' + value)
+    setNote((prevNote) => ({ ...prevNote, [name]: [value] }))
+  }
+
+  function onNoteSubmit(e) {
+    e.preventDefault()
+
+    setAllNotes((prevAllNotes) => {
+      const newNotes = { ...note }
+      newNotes.id = Date.now().toString()
+      return [newNotes, ...prevAllNotes]
+    })
+
+    setNote(startNote)
+  }
+
+  // Elements
+
+  const noteElements = allNotes.map((theNote) => {
+    return (
+      <div key={theNote.id} className="app-note">
+        <p>{theNote.content}</p>
+        <h5>{theNote.author}</h5>
+      </div>
+    )
+  })
 
   return (
     <section className="app-section">
       <div className="app-container">
         <h3>Smile</h3>
-        <p>
-          <textarea
-            rows="3"
-            placeholder="Write what you want..."
-            type="text"
-            name="content"
-            value={note.content}
-            onChange={onNoteValueChange}
-          />
-        </p>
-        <p>
-          <input
-            placeholder="Author name"
-            type="text"
-            name="author"
-            value={note.author}
-            onChange={onNoteValueChange}
-          />
-        </p>
+        <form onSubmit={onNoteSubmit}>
+          <p>
+            <textarea
+              rows="3"
+              placeholder="Write what you want..."
+              type="text"
+              name="content"
+              value={note.content}
+              onChange={onNoteValueChange}
+            />
+          </p>
+          <p>
+            <input
+              placeholder="Author name"
+              type="text"
+              name="author"
+              value={note.author}
+              onChange={onNoteValueChange}
+            />
+          </p>
+          <p>
+            <button type="submit">Add note</button>
+          </p>
+        </form>
+        <div className="app-notes"> {noteElements}</div>
       </div>
     </section>
   )
 }
 
 export default App
+
